4900c6efad8eccc6f5d295f57373ae2238299ca2
DenDev377 <putradendy486@gmail.com>
2026-09-03 11:03:16 +0700

    style: Menambah Padding pada footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
     return (
-        <footer className=" flex items-center bg-white border-t border-gray-200 px-8 py-4 shadow-sm">
+        <footer className=" flex items-center bg-white border-t border-gray-200 px-8 py-8 shadow-sm">
             <div className="flex items-center max-w-6xl mx-auto justify-center gap-3">
-                <p className="text-xs text-gray-400 font-medium">© 2026 Kejaksaan Negeri Oku . All rights reserved.</p>
+                <p className="text-sm text-gray-400 font-medium">© 2026 Kejaksaan Negeri Oku . All rights reserved.</p>
             </div>
         </footer>
     )
